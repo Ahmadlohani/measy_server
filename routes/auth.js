b8a763b5b1a3a52a2d120091d88c83b337d82684
felixCivalero <ah203076@gmail.com>
2022-08-29 16:47:53 +0500

    new

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,27 @@
 import express from "express";
-import {register, login, contact, currentUser, forgotPassword, cartData, cartPageData, productPayment, orderCompletionData} from "../controllers/auth";
+import {
+	register,
+	login,
+	contact,
+	currentUser,
+	forgotPassword,
+	cartData,
+	cartPageData,
+	productPayment,
+	orderCompletionData,
+} from "../controllers/auth";
 import { requireSignin } from "../middlewares";
 
 const router = express.Router();
+router.get("/", (req, res) => {
+	res.send("Server is running");
+});
 router.post("/register", register);
 router.post("/cartData", cartData);
-router.post("/paypal-transaction-complete", orderCompletionData);
+router.post(
+	"/paypal-transaction-complete",
+	orderCompletionData
+);
 router.post("/payment", productPayment);
 router.get("/cart-data/:_id", cartPageData);
 router.post("/login", login);
